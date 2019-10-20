8f3258a2ceef3261aaf2fe4c50d036b70dab96ef
pruth <pruth93@gmail.com>
2019-10-20 14:53:10 -0400

    Add Slider to Index.html

--- js/main.js
+++ js/main.js
@@ -2,13 +2,41 @@
  * Your JS code here
  */
 
-/**
- * Sample code
- */
-document.addEventListener('DOMContentLoaded', function() {
-  var ulElement = document.getElementById('links');
-  var liElement = document.createElement('li');
-  liElement.appendChild(document.createTextNode('created by main.js'));
+var slideIndex = 1;
+showSlides(slideIndex);
 
-  ulElement.appendChild(liElement);
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+/////////////////////////////////////
+
+document.addEventListener('DOMContentLoaded', function () {
+    var ulElement = document.getElementById('links');
+    var liElement = document.createElement('li');
+    liElement.appendChild(document.createTextNode('created by main.js'));
+
+    ulElement.appendChild(liElement);
 });
